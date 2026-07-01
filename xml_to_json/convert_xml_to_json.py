b54df1bec684db9d0195c3dd21b66ad260fb83cf
David Lee <davlee1972@yahoo.com>
2026-06-30 18:40:38 -0700

    Refactor XML to JSON conversion functions
    Refactor XML to JSON conversion logic, update function names, and improve error handling. Replace custom decimal serialization with a more comprehensive JSON decoder.

--- xml_to_json/convert_xml_to_json.py
+++ xml_to_json/convert_xml_to_json.py
@@ -1,9 +1,26 @@
+#
+# MIT License
+#
+# Copyright (c) 2026 David Lee
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 """
-Author: David Lee
-"""
-import xml.etree.cElementTree as ET
-import xmlschema
-from collections import OrderedDict
 import decimal
 import json
 import glob
@@ -11,88 +28,33 @@ from multiprocessing import Pool
 import subprocess
 import os
 import gzip
+import tarfile
 import logging
-import shutil
 import sys
+from zipfile import ZipFile
+
+import xmlschema
+from xmlschema.converters import ColumnarConverter
+from xmlschema import XMLResource
+
 
 _logger = logging.getLogger(__name__)
 _logger.setLevel(logging.DEBUG)
 
 
-def decimal_default(obj):
+def json_decoder(obj):
     """
     :param obj: python data
-    :return: a float
+    :return: converted type
     :raises:
     """
     if isinstance(obj, decimal.Decimal):
         return float(obj)
-    raise TypeError
-
-
-class ParqConverter(xmlschema.XMLSchemaConverter):
-    """
-    XML Schema based converter class for Parquet friendly json.
-    """
-
-    def __init__(self, namespaces=None, dict_class=None, list_class=None, text_key=None, attr_prefix=None, cdata_prefix=None, **kwargs):
-        """
-        :param namespaces: Map from namespace prefixes to URI.
-        :param dict_class: Dictionary class to use for decoded data. Default is `dict`.
-        :param list_class: List class to use for decoded data. Default is `list`.
-        :param text_key: is the key to apply to element"s decoded text data.
-        :param attr_prefix: controls the mapping of XML attributes, to the same name or \
-        with a prefix. If `None` the converter ignores attributes.
-        :param cdata_prefix: is used for including and prefixing the CDATA parts of a \
-        mixed content, that are labeled with an integer instead of a string. \
-        CDATA parts are ignored if this argument is `None`.
-        """
-
-        super().__init__(
-            namespaces, dict_class or OrderedDict, list_class,
-            attr_prefix=None, text_key=None, cdata_prefix=None
-        )
-
-    def element_decode(self, data, xsd_element):
-        """
-        :param data: Decoded ElementData from an Element node.
-        :param xsd_element: The `XsdElement` associated to decoded the data.
-        :return: A dictionary-based data structure containing the decoded data.
-        """
-
-        if data.attributes:
-            self.attr_prefix = data.tag
-            result_dict = self.dict([(k, v) for k, v in self.map_attributes(data.attributes)])
-        else:
-            result_dict = self.dict()
-
-        if xsd_element.type.is_simple() or xsd_element.type.has_simple_content():
-            result_dict[data.tag] = data.text if data.text is not None and data.text != "" else None
-
-        if data.content:
-            for name, value, xsd_child in self.map_content(data.content):
-                if value:
-                    if xsd_child.is_single():
-                        if xsd_child.type.is_simple() or xsd_child.type.has_simple_content():
-                            for k in value:
-                                result_dict[k] = value[k]
-                        else:
-                            result_dict[name] = value
-                    else:
-                        if (xsd_child.type.is_simple() or xsd_child.type.has_simple_content()) and len(xsd_element.findall("*")) == 1:
-                            try:
-                                result_dict.append(value)
-                            except AttributeError:
-                                result_dict = self.list([value])
-                        else:
-                            try:
-                                result_dict[name].append(value)
-                            except KeyError:
-                                result_dict[name] = self.list([value])
-                            except AttributeError:
-                                result_dict[name] = self.list([value])
-
-        return result_dict
+    if isinstance(obj, datetime):
+        return obj.strftime('%Y-%m-%d %H:%M:%S.%f')
+    if isinstance(obj, set):
+        return list(obj)
+    raise TypeError(repr(obj) + " is not JSON serializable")
 
 
 def open_file(zip, filename):
@@ -107,116 +69,127 @@ def open_file(zip, filename):
         return open(filename, "wb")
 
 
-def parse_file(xml_file, output_file, xsd_file, output_format, zip, xpath):
+def parse_xml(xml_file, lazy, json_file, xml_schema, xpath, xpath_items, output_format, processed, from_zip):
     """
     :param xml_file: xml file
+    :param xpath: whether to parse a specific xml path
+    :param json_file: json file
+    :param my_schema: xmlschema object
+    :param output_format: jsonl or json
+    :param from_zip: if data is from a file in a zip archive
+    :return: data found and processed
+    """
+
+    xml_file_resource = XMLResource(xml_file, lazy=lazy, thin_lazy=True)
+    
+    if xpath:
+        xml_iter_decode = xml_schema.iter_decode(xml_file_resource, path=xpath)
+    else:
+        xml_iter_decode = xml_schema.iter_decode(xml_file_resource)
+
+    if xpath_items:
+        root = xpath_items[-1]
+    else:
+        root = None
+
+    for xml_dict in xml_iter_decode:
+        if root:
+            xml_dict = xml_dict[root]
+        try:
+            xml_json = json.dumps(xml_dict, default=json_decoder)
+        except Exception as ex:
+            _logger.debug(ex)
+            pass
+        if len(xml_json) > 0:
+            if not processed:
+                processed = True
+                json_file.write(bytes(xml_json, "utf-8"))
+            else:
+                if output_format == "json":
+                    json_file.write(bytes("," + os.linesep + xml_json, "utf-8"))
+                else:
+                    json_file.write(bytes(os.linesep + xml_json, "utf-8"))
+    return processed
+
+
+def parse_file(input_file, output_file, xsd_file, output_format, zip, xpath, target_path, delete_xml):
+    """
+    :param input_file: input file
     :param output_file: output file
     :param xsd_file: xsd file
     :param output_format: jsonl or json
     :param zip: zip save file
     :param xpath: whether to parse a specific xml path
+    :param target_path: directory to save file
+    :param delete_xml: optional delete xml file after converting
     """
 
     _logger.debug("Generating schema from " + xsd_file)
 
-    my_schema = xmlschema.XMLSchema(xsd_file, converter=ParqConverter)
+    xml_schema = xmlschema.XMLSchema(xsd_file, converter=ColumnarConverter)
 
-    _logger.debug("Parsing " + xml_file)
+    _logger.debug("Parsing " + input_file)
 
     _logger.debug("Writing to file " + output_file)
 
-    context = ET.iterparse(xml_file, events=("start", "end"))
-
-    xpath_list = None
-
     if xpath:
-        xpath_list = xpath.split("/")
-        del xpath_list[0]
-        # If xpath is just the root then use other routine
-        if len(xpath_list) == 1:
-            xpath_list = None
-
-    if xpath_list:
-
-        root = ET.XML("<" + "><".join(xpath_list[:-1]) + "></" + "></".join(xpath_list[:-1][::-1]) + ">")
-        parent = root
-        for i in xpath_list[:-2]:
-            parent = parent[0]
-
-        first_record = True
-        elem_active = False
-        currentxpath = []
-
-        isjsonarray = False
-        xsd_elem = my_schema.find(xpath)
-        if xsd_elem.max_occurs is None or xsd_elem.max_occurs > 1:
-            isjsonarray = True
-
-        with open_file(zip, output_file) as json_file:
-
-            # 2nd pass open xml file and get elements in the list
-            _logger.debug("Parsing " + xpath_list[-1] + " from " + xml_file)
-
-            if isjsonarray and output_format != "jsonl":
-                json_file.write(bytes("[\n", "utf-8"))
-
-            # Start parsing items out of XML
-            for event, elem in context:
-                if event == "start":
-                    currentxpath.append(elem.tag)
-                    if currentxpath == xpath_list:
-                        elem_active = True
-                if event == "end":
-                    if currentxpath == xpath_list:
-                        elem_active = False
-                        parent.append(elem)
-                        my_dict = my_schema.to_dict(root, path=xpath)
-                        parent.remove(elem)
-                        my_json = json.dumps(my_dict, default=decimal_default)
-                        if first_record:
-                            first_record = False
-                            json_file.write(bytes(my_json, "utf-8"))
-                        else:
-                            if output_format == "jsonl":
-                                json_file.write(bytes("\n" + my_json, "utf-8"))
-                            else:
-                                json_file.write(bytes(",\n" + my_json, "utf-8"))
-
-                        # my_json = json.dumps(my_dict, default=decimal_default)
-                        # _logger.info(my_json)
-                        # _logger.info("\n")
-                        # input("Press Enter to continue...")
-
-                    if not elem_active:
-                        elem.clear()
-
-                    del currentxpath[-1]
-
-            if isjsonarray and output_format != "jsonl":
-                json_file.write(bytes("\n]", "utf-8"))
-
-        # Remove file if no json is generated
-        if first_record:
-            os.remove(output_file)
-            return
-
+        xpath_items = xpath.split("/")
+        lazy = len(xpath_items) - 2
+        if lazy < 0:
+            lazy = False
     else:
-        elem, root = next(context)
-        my_dict = my_schema.to_dict(xml_file)
-        my_json = '{"' + root.tag + '": ' + json.dumps(my_dict, default=decimal_default) + "}"
-        with open_file(zip, output_file) as json_file:
-            json_file.write(bytes(my_json, "utf-8"))
+        lazy = False
+        xpath_items = []
 
-    del context
+    processed = False
 
-    _logger.debug("Completed " + xml_file)
+    with open_file(zip, output_file) as json_file:
+
+        if input_file.endswith((".zip", ".tar.gz")) and output_format == "json":
+            json_file.write(bytes("[" + os.linesep, "utf-8"))
+
+        if input_file.endswith(".tar.gz"):
+            zip_file = tarfile.open(input_file, 'r')
+            zip_file_list = zip_file.getmembers()
+
+            for member in zip_file_list:
+                with zip_file.extractfile(member) as xml_file:
+                    processed = parse_xml(xml_file, lazy, json_file, xml_schema, xpath, xpath_items, output_format, processed, from_zip=True)
+
+        elif input_file.endswith(".zip"):
+            zip_file = ZipFile(input_file, 'r')
+            zip_file_list = zip_file.infolist()
+
+            for i in range(len(zip_file_list)):
+                with zip_file.open(zip_file_list[i].filename) as xml_file:
+                    processed = parse_xml(xml_file, lazy, json_file, xml_schema, xpath, xpath_items, output_format, processed, from_zip=True)
+        
+        elif input_file.endswith(".gz"):
+            with gzip.open(input_file) as xml_file:
+                processed = parse_xml(xml_file, lazy, json_file, xml_schema, xpath, xpath_items, output_format, processed, from_zip=False)
+
+        else:
+            processed = parse_xml(input_file, lazy, json_file, xml_schema, xpath, xpath_items, output_format, processed, from_zip=False)
+
+        if input_file.endswith((".zip", ".tar.gz")) and output_format == "json":
+            json_file.write(bytes(os.linesep + "]", "utf-8"))
+
+    # Remove file if no json is generated
+    if not processed:
+        os.remove(output_file)
+        _logger.debug("No data found in " + input_file)
+        return
+
+    if delete_xml:
+        os.remove(input_file)
+
+    _logger.debug("Completed " + input_file)
 
 
-def convert_xml_to_json(xsd_file=None, output_format="jsonl", server=None, target_path=None, zip=False, xpath=None, multi=1, no_overwrite=False, verbose="DEBUG", log=None, xml_files=None):
+def convert_xml(xsd_file=None, output_format="jsonl", target_path=None, zip=False, xpath=None, multi=1, no_overwrite=False, verbose="DEBUG", log=None, delete_xml=None, xml_files=None):
     """
     :param xsd_file: xsd file name
     :param output_format: jsonl or json
-    :param server: optional server with hadoop client installed if current server does not have hadoop installed
     :param target_path: directory to save file
     :param zip: zip save file
     :param xpath: whether to parse a specific xml path
@@ -224,6 +197,7 @@ def convert_xml_to_json(xsd_file=None, output_format="jsonl", server=None, targe
     :param no_overwrite: overwrite target file
     :param verbose: stdout log messaging level
     :param log: optional log file
+    :param delete_xml: optional delete xml file after converting
     :param xml_files: list of xml_files
 
     """
@@ -244,33 +218,20 @@ def convert_xml_to_json(xsd_file=None, output_format="jsonl", server=None, targe
 
     _logger.info("Parsing XML Files..")
 
-    if target_path:
-        if target_path.startswith("hdfs:"):
-            if server:
-                if subprocess.call(["ssh", server, "hadoop fs -test -e " + target_path]) != 0:
-                    _logger.error("invalid target_path: " + target_path + " using hadoop server: " + server)
-                    sys.exit(1)
-            elif shutil.which("hadoop"):
-                if subprocess.call(["hadoop", "fs", "-test", "-e", target_path]) != 0:
-                    _logger.error("invalid target_path: " + target_path)
-                    sys.exit(1)
-            else:
-                _logger.error("no hadoop client found")
-                sys.exit(1)
-        else:
-            if not os.path.exists(target_path):
-                _logger.error("invalid target_path specified")
-                sys.exit(1)
+    if target_path and not os.path.exists(target_path):
+        _logger.error("invalid target_path specified")
+        sys.exit(1)
 
     # open target files
     file_list = list(set([f for _files in [glob.glob(xml_files[x]) for x in range(0, len(xml_files))] for f in _files]))
     file_count = len(file_list)
 
-    parse_queue_pool = Pool(processes=multi)
+    if multi > 1:
+        parse_queue_pool = Pool(processes=multi)
 
     _logger.info("Processing " + str(file_count) + " files")
 
-    if len(file_list) <= 1000:
+    if 1 < len(file_list) <= 1000:
         file_list.sort(key=os.path.getsize, reverse=True)
         _logger.info("Parsing files in the following order:")
         _logger.info(file_list)
@@ -278,52 +239,45 @@ def convert_xml_to_json(xsd_file=None, output_format="jsonl", server=None, targe
     for filename in file_list:
 
         path, xml_file = os.path.split(os.path.realpath(filename))
+        
+        output_file = xml_file
 
-        if xml_file.endswith(".xml"):
-            if output_format == "jsonl":
-                output_file = xml_file[:-4] + ".jsonl"
-            else:
-                output_file = xml_file[:-4] + ".json"
+        if output_file.endswith(".gz"):
+            output_file = output_file[:-3]
+
+        if output_file.endswith(".tar"):
+            output_file = output_file[:-4]
+
+        if output_file.endswith(".zip"):
+            output_file = output_file[:-4]
+
+        if output_file.endswith(".xml"):
+            output_file = output_file[:-4]
+
+        if output_format == "jsonl":
+            output_file = output_file + ".jsonl"
         else:
-            if output_format == "jsonl":
-                output_file = xml_file + ".jsonl"
-            else:
-                output_file = xml_file + ".json"
+            output_file = output_file + ".json"
 
         if zip:
             output_file = output_file + ".gz"
 
-        if not target_path:
-            output_file = os.path.join(path, output_file)
-            if no_overwrite and os.path.isfile(output_file):
-                _logger.debug("No overwrite. Skipping " + xml_file)
-                continue
-        elif target_path.startswith("hdfs:"):
-            if no_overwrite and subprocess.call(["hadoop", "fs", "-test", "-e", os.path.join(target_path, output_file)]) == 0:
-                _logger.debug("No overwrite. Skipping " + xml_file)
-                continue
-            output_file = os.path.join(path, output_file)
-        else:
+        if target_path:
             output_file = os.path.join(target_path, output_file)
             if no_overwrite and os.path.isfile(output_file):
                 _logger.debug("No overwrite. Skipping " + xml_file)
                 continue
+        else:
+            output_file = os.path.join(path, output_file)
+            if no_overwrite and os.path.isfile(output_file):
+                _logger.debug("No overwrite. Skipping " + xml_file)
+                continue
 
-        parse_queue_pool.apply_async(parse_file, args=(filename, output_file, xsd_file, output_format, zip, xpath), error_callback=_logger.info)
-        # parse_file(parse_file(xml_file, output_file, xsd_file, output_format, zip, xpath)
+        if multi > 1:
+            parse_queue_pool.apply_async(parse_file, args=(filename, output_file, xsd_file, output_format, zip, xpath, target_path, delete_xml), error_callback=_logger.info)
+        else:
+            parse_file(filename, output_file, xsd_file, output_format, zip, xpath, target_path, delete_xml)
 
-        if target_path and target_path.startswith("hdfs:") and os.path.isfile(output_file):
-            _logger.debug("Moving " + output_file + " to " + target_path)
-            if server:
-                if subprocess.call(["ssh", server, "hadoop fs -put -f " + output_file + " " + target_path]) != 0:
-                    _logger.error("invalid target_path specified")
-                    sys.exit(1)
-            else:
-                if subprocess.call(["hadoop", "fs", "-put", "-f", output_file, target_path]) != 0:
-                    _logger.error("invalid target_path specified")
-                    sys.exit(1)
-
-            os.remove(output_file)
-
-    parse_queue_pool.close()
-    parse_queue_pool.join()
+    if multi > 1:
+        parse_queue_pool.close()
+        parse_queue_pool.join()
